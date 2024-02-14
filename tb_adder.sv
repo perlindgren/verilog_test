e3aefbd382c8ce8679cea85e8dcaa67c4beedca3
Per Lindgren <per.lindgren@ltu.se>
2024-02-14 13:48:26 +0100

    nice for demo

--- tb_adder.sv
+++ tb_adder.sv
@@ -26,14 +26,18 @@ module tb_adder;
     b = 0;
     c_in = 0;
 
-    $monitor("a=0x%0h b=0x%0h c_in=0x%0h c_out=0x%0h sum=0x%0h", a, b, c_in, c_out, sum);
 
     for (i = 0; i < 5; i = i + 1) begin
       r = $random;
-      #10 a = r[3:0];
+      a = r[3:0];
       b = r[7:4];
       c_in = r[8];
+      #10 $display("a=0x%0h b=0x%0h c_in=0x%0h c_out=0x%0h sum=0x%0h", a, b, c_in, c_out, sum);
+      // assert ( a + b + c_in  == {c_out, sum}) $display ("OK. A equals B");
+      // else $error("It's gone wrong");
     end
+
+    #10 $finish;
 
   end
 
