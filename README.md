067e835e7f269a743ae8a071d9dad46d9206a7ba
Per Lindgren <per.lindgren@ltu.se>
2024-02-14 11:39:55 +0100

    still pest

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ verilator --cc adder.sv
 ```
 
 ```shell
-verilator --binary adder.sv tb_adder.sv --top-module tb_adder.sv 
+verilator --binary adder.sv tb_adder.sv --top-module tb_adder
 ```
--- tb_adder.sv
+++ tb_adder.sv
@@ -1,9 +1,12 @@
 module tb_adder;
 
-  reg [3:0] a;
-  reg [3:0] b;
-  reg c_in;
-  wire [3:0] sum;
+  logic [3:0] a;
+  logic [3:0] b;
+  logic c_in;
+
+  logic c_out;
+  logic [3:0] sum;
+
   integer i;
   adder fa0 (
       a,
@@ -14,15 +17,15 @@ module tb_adder;
   );
 
   initial begin
-    a <= 0;
-    b <= 0;
-    c_in <= 0;
+    a = 0;
+    b = 0;
+    c_in = 0;
     $monitor("a=0x%0h b=0x%0h c_in=0x%0h c_out=0x%0h sum=0x%0h", a, b, c_in, c_out, sum);
 
     for (i = 0; i < 5; i = i + 1) begin
-      #10 a <= $random;
-      b <= $random;
-      c_in <= $random;
+      #10 a = $random;
+      b = $random;
+      c_in = $random;
     end
   end
 
